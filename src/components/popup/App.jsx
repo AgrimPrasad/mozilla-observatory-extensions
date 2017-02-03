1d9cca420be57daf14a2afdb54696886b295c074
Agrim Prasad <agrimprasad@gmail.com>
2017-02-03 23:25:10 +0800

    Get current tab url and extract its hostname for display in popup

--- src/components/popup/App.jsx
+++ src/components/popup/App.jsx
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import URL from 'url-parse';
 
 import * as actionCreators from '../../actions/actionCreators';
 
 class App extends Component {
   componentDidMount() {
-    this.props.selectHost('rediff.com');
+    chrome.tabs.query({currentWindow: true, active: true}, (tabs) => {
+        const hostname = new URL(tabs[0].url).hostname;
+        this.props.selectHost(hostname);
+    });
   }
 
   render() {
     return (
-      <div>
-        <h2>
-          Current Host: {this.props.selectedHost}
-        </h2>
-      </div>
+        <div>
+          <h2>
+            Current Host: {this.props.selectedHost}
+          </h2>
+        </div>
       );
   }
 }
